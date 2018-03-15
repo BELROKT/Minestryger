f99865e09a8ca43ba0425ccac61f5daaab573892
Armienn <nigathr@gmail.com>
2018-03-15 17:10:01 +0100

    Satte alting ind i en klasse

--- script.js
+++ script.js
@@ -1,19 +1,196 @@
 "use strict"
 
-var canvas = document.getElementById("kanvas")
-var context = canvas.getContext("2d")
-var xtal = 0
-var ytal = 0
-var width = 30
-var height = 16
-var size = 20
+class Game {
+    constructor() {
+        this.canvas = document.getElementById("kanvas")
+        this.context = this.canvas.getContext("2d")
+        this.width = 30
+        this.height = 16
+        this.size = 20
+        this.seconds = 0
+        this.bombetal = 0
+        this.timerId
+        this.fields = []
 
-canvas.width = width * (size + 1)
-canvas.height = height * (size + 1)
-context.fillStyle = "black"
-context.fillRect(0, 0, canvas.width, canvas.height)
-context.font = size + "px serif"
-context.textAlign = "center"
+        this.canvas.width = this.width * (this.size + 1)
+        this.canvas.height = this.height * (this.size + 1)
+        this.context.font = this.size + "px serif"
+        this.context.textAlign = "center"
+
+        this.canvas.addEventListener("click", (event) => { this.clickBox(event) })
+    }
+
+    clearMap() {
+        this.seconds = 0
+        this.bombetal = 0
+        this.context.fillStyle = "black"
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+        this.fields = []
+        var xtal = 0
+        var ytal = 0
+        while (ytal < this.height) {
+            var subList = []
+            this.fields.push(subList)
+            while (xtal < this.width) {
+                this.drawBox(xtal, ytal, "", "grey")
+                this.fields[ytal].push(new Field())
+                xtal += 1
+            }
+            ytal += 1
+            xtal = 0
+        }
+    }
+
+    drawBox(x, y, text, color) {
+        this.context.fillStyle = color
+        this.context.fillRect((this.size + 1) * x, (this.size + 1) * y, this.size, this.size)
+        this.context.fillStyle = "black"
+        this.context.fillText(text, (this.size + 1) * x + 0.5 * this.size, (this.size + 1) * y + 0.8 * this.size)
+    }
+
+    clickBox(event) {
+        var rect = this.canvas.getBoundingClientRect()
+        var mx = event.clientX - rect.left
+        var my = event.clientY - rect.top
+        if (((mx + 1) % (this.size + 1) == 0) || ((my + 1) % (this.size + 1) == 0)) {
+            return
+        }
+        var x = Math.floor(mx / (this.size + 1))
+        var y = Math.floor(my / (this.size + 1))
+        if (this.bombetal == 0) {
+            this.firstClick(x, y)
+            this.updateCounts()
+        }
+        this.revealField(x, y)
+    }
+
+
+    firstClick(mx, my) {
+        this.fields[my][mx].bomb = true
+        while (this.bombetal < 99) {
+            var x = randomInteger(this.width)
+            var y = randomInteger(this.height)
+            if (this.fields[y][x].bomb == true) {
+                continue
+            }
+            this.fields[y][x].bomb = true
+            this.bombetal += 1
+        }
+        this.fields[my][mx].bomb = false
+        this.timer()
+        this.timerId = setInterval(() => { this.timer() }, 1000)
+    }
+
+    revealField(x, y) {
+        if ((x >= this.width) || (y >= this.height)) {
+            return
+        }
+        var field = this.fields[y][x]
+        if (field.revealed) {
+            return
+        }
+        if (field.bomb) {
+            this.drawBox(x, y, "B", "red")
+        }
+        else {
+            this.drawBox(x, y, field.surroundingBombs, "white")
+        }
+        field.revealed = true
+        if (field.bomb) {
+            clearInterval(this.timerId)
+            this.forFieldInMap((i, j, otherField) => {
+                if (otherField.bomb) {
+                    this.revealField(i, j)
+                }
+            })
+        }
+        else if (field.surroundingBombs == 0) {
+            this.forSurroundingFields(x, y, (xm, yn, otherField) => {
+                this.revealField(xm, yn)
+            })
+        }
+        var everythingRevealed = true
+        this.forFieldInMap((i, j, otherField) => {
+            if ((!otherField.revealed) && (!otherField.bomb)) {
+                everythingRevealed = false
+            }
+        })
+        if (everythingRevealed) {
+            clearInterval(this.timerId)
+        }
+    }
+
+    revealMap() {
+        this.forFieldInMap((x, y, field) => { this.revealField(x, y, field) })
+    }
+
+    updateCounts() {
+        this.forFieldInMap((x, y, field) => { this.countBombs(x, y, field) })
+    }
+
+    forFieldInMap(callback) {
+        var x = 0
+        var y = 0
+        while (y < this.height) {
+            while (x < this.width) {
+                callback(x, y, this.fields[y][x])
+                x = x + 1
+            }
+            y = y + 1
+            x = 0
+        }
+    }
+
+    forSurroundingFields(x, y, callback) {
+        var m = -1
+        var n = -1
+        while (n < 2) {
+            if ((y + n < 0) || (y + n >= this.height)) {
+                n += 1
+                continue
+            }
+            while (m < 2) {
+                if ((m == 0) && (n == 0)) {
+                    m += 1
+                    continue
+                }
+                if ((x + m < 0) || (x + m >= this.width)) {
+                    m += 1
+                    continue
+                }
+                callback(x + m, y + n, this.fields[y + n][x + m])
+                m += 1
+            }
+            n += 1
+            m = -1
+        }
+    }
+
+    countBombs(x, y) {
+        if (this.fields[y][x].bomb) {
+            return
+        }
+        this.forSurroundingFields(x, y, (xm, yn, field) => {
+            if (field.bomb) {
+                this.fields[y][x].surroundingBombs += 1
+            }
+        })
+    }
+
+    timer() {
+        document.getElementById("timer").innerHTML = this.seconds
+        this.seconds += 1
+    }
+}
+
+class Field {
+    constructor() {
+        this.bomb = false
+        this.revealed = false
+        this.surroundingBombs = 0
+        this.locked = false
+    }
+}
 
 function randomInteger(number) {
     return Math.floor(Math.random() * number)
@@ -23,182 +200,5 @@ function randomColor() {
     return "rgb(" + randomInteger(255) + "," + randomInteger(255) + "," + randomInteger(255) + ")"
 }
 
-function drawBox(x, y, text, color) {
-    context.fillStyle = color
-    context.fillRect((size + 1) * x, (size + 1) * y, size, size)
-    context.fillStyle = "black"
-    context.fillText(text, (size + 1) * x + 0.5 * size, (size + 1) * y + 0.8 * size)
-}
-
-function clickBox(event) {
-    var rect = canvas.getBoundingClientRect()
-    var mx = event.clientX - rect.left
-    var my = event.clientY - rect.top
-    if (((mx + 1) % (size + 1) == 0) || ((my + 1) % (size + 1) == 0)) {
-        return
-    }
-    var x = Math.floor(mx / (size + 1))
-    var y = Math.floor(my / (size + 1))
-    if (bombetal == 0) {
-        firstClick(x, y)
-        updateCounts()
-    }
-    revealField(x, y)
-}
-
-function revealField(x, y) {
-    if ((x >= width) || (y >= height)) {
-        return
-    }
-    var field = fields[y][x]
-    if (field.revealed) {
-        return
-    }
-    if (field.bomb) {
-        drawBox(x, y, "B", "red")
-    }
-    else {
-        drawBox(x, y, field.surroundingBombs, "white")
-    }
-    field.revealed = true
-    if (field.bomb) {
-        clearInterval(timerId)
-        forFieldInMap(function (i, j, otherField) {
-            if (otherField.bomb) {
-                revealField(i, j)
-            }
-        })
-    }
-    else if (field.surroundingBombs == 0) {
-        forSurroundingFields(x, y, function (xm, yn, otherField) {
-            revealField(xm, yn)
-        })
-    }
-    var everythingRevealed = true
-    forFieldInMap(function (i, j, otherField) {
-        if ((!otherField.revealed) && (!otherField.bomb)) {
-            everythingRevealed = false
-        }
-
-    })
-    if (everythingRevealed) {
-        clearInterval(timerId)
-    }
-}
-
-function randomBomb() {
-    if (Math.random() < (99 / 480)) {
-        return "B"
-    }
-    else {
-        return 0
-    }
-}
-
-function revealMap() {
-    forFieldInMap(revealField)
-}
-
-function updateCounts() {
-    forFieldInMap(countBombs)
-}
-
-function forFieldInMap(callback) {
-    var x = 0
-    var y = 0
-    while (y < height) {
-        while (x < width) {
-            callback(x, y, fields[y][x])
-            x = x + 1
-        }
-        y = y + 1
-        x = 0
-    }
-}
-
-function forSurroundingFields(x, y, callback) {
-    var m = -1
-    var n = -1
-    while (n < 2) {
-        if ((y + n < 0) || (y + n >= height)) {
-            n += 1
-            continue
-        }
-        while (m < 2) {
-            if ((m == 0) && (n == 0)) {
-                m += 1
-                continue
-            }
-            if ((x + m < 0) || (x + m >= width)) {
-                m += 1
-                continue
-            }
-            callback(x + m, y + n, fields[y + n][x + m])
-            m += 1
-        }
-        n += 1
-        m = -1
-    }
-}
-
-function countBombs(x, y) {
-    if (fields[y][x].bomb) {
-        return
-    }
-    forSurroundingFields(x, y, function (xm, yn, field) {
-        if (field.bomb) {
-            fields[y][x].surroundingBombs += 1
-        }
-    })
-}
-
-function newField() {
-    return {
-        bomb: false,
-        revealed: false,
-        surroundingBombs: 0,
-        locked: false
-    }
-}
-
-var fields = []
-
-while (ytal < height) {
-    var subList = []
-    fields.push(subList)
-    while (xtal < width) {
-        drawBox(xtal, ytal, "", "grey")
-        fields[ytal].push(newField())
-        xtal += 1
-    }
-    ytal += 1
-    xtal = 0
-}
-
-var bombetal = 0
-var timerId
-
-function firstClick(mx, my) {
-    fields[my][mx].bomb = true
-    while (bombetal < 99) {
-        var x = randomInteger(width)
-        var y = randomInteger(height)
-        if (fields[y][x].bomb == true) {
-            continue
-        }
-        fields[y][x].bomb = true
-        bombetal += 1
-    }
-    fields[my][mx].bomb = false
-    timer()
-    timerId = setInterval(timer, 1000)
-}
-
-var seconds = 0
-
-function timer() {
-    document.getElementById("timer").innerHTML = seconds
-    seconds += 1
-}
-
-canvas.addEventListener("click", clickBox)
+var game = new Game()
+game.clearMap()
